7e6f0cedfbcef9604575f6d596f1740a0a1661bb
viciouscal <78273450+viciouscal@users.noreply.github.com>
2026-02-06 09:57:06 +0300

    Update index.tsx

--- src/plugins/platformSpoofer/index.tsx
+++ src/plugins/platformSpoofer/index.tsx
@@ -9,7 +9,7 @@ import "plugins/_misc/styles.css";
 import { definePluginSettings } from "@api/Settings";
 import { Devs } from "@utils/constants";
 import definePlugin, { OptionType } from "@utils/types";
-import { Forms } from "@webpack/common";
+import { Forms, UserStore } from "@webpack/common";
 
 const settings = definePluginSettings({
     platform: {
@@ -42,32 +42,46 @@ export default definePlugin({
     name: "PlatformSpoofer",
     description: "Spoof what platform or device you're on",
     authors: [Devs.viciouscal],
-    settingsAboutComponent: () => <>
-    <Forms.FormText className="plugin-warning">
-    Yousef. is not responsible for u violation of the laws :3
-    </Forms.FormText>
-</>,
     settings: settings,
     patches: [
         {
             find: "_doIdentify(){",
             replacement: {
                 match: /(\[IDENTIFY\].*let.{0,5}=\{.*properties:)(.*),presence/,
-                replace: "$1{...$2,...$self.getPlatform()},presence"
+                replace: "$1{...$2,...$self.getPlatform(true)},presence"
+            }
+        },
+        {
+            find: "#{intl::POPOUT_STAY_ON_TOP}),icon:",
+            replacement: {
+                match: /(?<=CallTile.{0,15}\.memo\((\i)=>\{)/,
+                replace: "$1.platform = $self.getPlatform(false, $1?.participantUserId)?.vcIcon || $1?.platform;"
+            }
+        },
+        {
+            find: '("AppSkeleton");',
+            replacement: {
+                match: /(?<=\.isPlatformEmbedded.{0,50}\i\)\)\}.{0,30})\i\?\i\.\i\.set\(.{0,10}:/,
+                replace: ""
             }
         }
     ],
-    getPlatform: () => {
-        switch (settings.store.platform ?? "desktop") {
-            case "desktop":
-                return { browser: "Discord Client" };
-            case "web":
-                return { browser: "Chrome" };
-            case "mobile":
-                return { browser: "Discord iOS" };
-            case "embedded":
-                return { browser: "Discord Embedded" };
+    getPlatform(bypass, userId?: any) {
+        const platform = settings.store.platform ?? "desktop";
+
+        if (bypass || userId === UserStore.getCurrentUser().id) {
+            switch (platform) {
+                case "desktop":
+                    return { browser: "Discord Client", vcIcon: 0 };
+                case "web":
+                    return { browser: "Chrome", vcIcon: 0 };
+                case "mobile":
+                    return { browser: "Discord iOS", vcIcon: 1 };
+                case "embedded":
+                    return { browser: "Discord Embedded" };
+            }
         }
 
+        return null;
     }
 });
