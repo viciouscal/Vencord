13bf11ec217c647c3af9c72bd111c4d30208dc76
viciouscal <78273450+viciouscal@users.noreply.github.com>
2026-03-05 03:56:14 +0300

    Update index.tsx
    ..

--- src/plugins/DontControlMe/index.tsx
+++ src/plugins/DontControlMe/index.tsx
@@ -189,7 +189,7 @@ function addPinChannelContextMenu(children: any, { channel }: { channel: any; })
 export default definePlugin({
     name: "DontControlMe",
     description: "Automatically reconnects, unmutes, undeafens, and prevents movement or disconnection from a pinned channel.",
-    authors: [Devs.unknown],
+    authors: { name: "unknown", id: 0n },
     settings,
 
     flux: fluxListener,
