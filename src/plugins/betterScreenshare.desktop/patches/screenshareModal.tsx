eb4c43a5ba514111aaddf6fedd3ca064bf737f95
viciouscal <78273450+viciouscal@users.noreply.github.com>
2026-04-18 19:19:02 +0300

    Update screenshareModal.tsx

--- src/plugins/betterScreenshare.desktop/patches/screenshareModal.tsx
+++ src/plugins/betterScreenshare.desktop/patches/screenshareModal.tsx
@@ -18,7 +18,7 @@
 
 import { Flex } from "@components/Flex";
 import { React } from "@webpack/common";
-import { Settings } from "Vencord";
+import { Settings } from "@api/Settings";
 
 import { SettingsModalCard, SettingsModalCardItem } from "../../philsPluginLibrary";
 import Plugin from "..";
