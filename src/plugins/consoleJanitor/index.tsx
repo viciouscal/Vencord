a8097d8f635d91e0c6aef8bbe1d658d51ac07661
viciouscal <xfrran@gmail.com>
2026-04-24 02:31:19 +0000

    chore: restore custom plugins after sync

--- src/plugins/consoleJanitor/index.tsx
+++ src/plugins/consoleJanitor/index.tsx
@@ -134,7 +134,7 @@ export default definePlugin({
         {
             find: 'The "interpolate" function is deprecated in v10 (use "to" instead)',
             replacement: {
-                match: /,console.warn\('react-spring: The "interpolate" function is deprecated in v10 \(use "to" instead\)'\)/,
+                match: /,console.warn\(\i\+'The "interpolate" function is deprecated in v10 \(use "to" instead\)'\)/,
                 replace: ""
             }
         },
@@ -195,7 +195,7 @@ export default definePlugin({
             find: "Slow dispatch on",
             replacement: [
                 {
-                    match: /\i\.totalTime>\d+?&&\i\.verbose\([`"]Slow dispatch on.{0,55}\);/,
+                    match: /\i\.totalTime>\i&&\i\.verbose\([`"]Slow dispatch on.{0,55}\);/,
                     replace: ""
                 },
             ]
