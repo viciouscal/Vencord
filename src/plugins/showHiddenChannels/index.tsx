a39270c494ca8cdcb4885eeb1e0fbfd77abf2539
viciouscal <78273450+viciouscal@users.noreply.github.com>
2026-04-25 05:32:33 +0300

    Update index.tsx

--- src/plugins/showHiddenChannels/index.tsx
+++ src/plugins/showHiddenChannels/index.tsx
@@ -20,40 +20,51 @@ import "./style.css";
 
 import { definePluginSettings } from "@api/Settings";
 import ErrorBoundary from "@components/ErrorBoundary";
-import { Devs } from "@utils/constants";
+import { Devs, EquicordDevs } from "@utils/constants";
 import { classNameFactory } from "@utils/css";
 import { classes } from "@utils/misc";
 import definePlugin, { OptionType } from "@utils/types";
 import type { Channel, Role } from "@vencord/discord-types";
-import { findCssClassesLazy } from "@webpack";
 import { ChannelStore, PermissionsBits, PermissionStore, Tooltip } from "@webpack/common";
 
-import HiddenChannelLockScreen from "./components/HiddenChannelLockScreen";
+import HiddenChannelLockScreen, { setChannelBeginHeader } from "./components/HiddenChannelLockScreen";
 
 export const cl = classNameFactory("vc-shc-");
 
-const ChannelListClasses = findCssClassesLazy("modeSelected", "modeMuted", "unread", "icon");
-
 const enum ShowMode {
     LockIcon,
-    HiddenIconWithMutedStyle
+    LockIconRight,
+    EyeIconRight,
+}
+
+const enum ChannelStyle {
+    Classic,
+    Muted,
+    Unread,
+    MutedUnread,
 }
 
 const CONNECT = 1n << 20n;
 
 export const settings = definePluginSettings({
-    hideUnreads: {
-        description: "Hide Unreads",
-        type: OptionType.BOOLEAN,
-        default: true,
+    channelStyle: {
+        description: "The style used to display hidden channels.",
+        type: OptionType.SELECT,
+        options: [
+            { label: "Classic", value: ChannelStyle.Classic, default: true },
+            { label: "Muted", value: ChannelStyle.Muted },
+            { label: "Show Unreads", value: ChannelStyle.Unread },
+            { label: "Muted and Show Unreads", value: ChannelStyle.MutedUnread }
+        ],
         restartNeeded: true
     },
     showMode: {
         description: "The mode used to display hidden channels.",
         type: OptionType.SELECT,
         options: [
-            { label: "Plain style with Lock Icon instead", value: ShowMode.LockIcon, default: true },
-            { label: "Muted style with hidden eye icon on the right", value: ShowMode.HiddenIconWithMutedStyle },
+            { label: "Lock Icon replacing channel icon", value: ShowMode.LockIcon, default: true },
+            { label: "Eye icon on the right", value: ShowMode.EyeIconRight },
+            { label: "Lock icon on the right", value: ShowMode.LockIconRight }
         ],
         restartNeeded: true
     },
@@ -71,7 +82,9 @@ function isUncategorized(objChannel: { channel: Channel; comparator: number; }) 
 export default definePlugin({
     name: "ShowHiddenChannels",
     description: "Show channels that you do not have access to view.",
-    authors: [Devs.BigDuck, Devs.AverageReactEnjoyer, Devs.D3SOX, Devs.Ven, Devs.Nuckyz, Devs.Nickyux, Devs.dzshn],
+    tags: ["Servers", "Utility"],
+    authors: [Devs.BigDuck, Devs.AverageReactEnjoyer, Devs.D3SOX, Devs.Ven, Devs.Nuckyz, Devs.Nickyux, Devs.dzshn, EquicordDevs.Oggetto],
+    isModified: true,
     settings,
 
     patches: [
@@ -162,17 +175,30 @@ export default definePlugin({
         },
         {
             find: "UNREAD_IMPORTANT:",
-            predicate: () => settings.store.showMode === ShowMode.HiddenIconWithMutedStyle,
+            predicate: () => settings.store.showMode !== ShowMode.LockIcon,
+            replacement: [
+                // Add the hidden eye icon if the channel is hidden
+                {
+                    predicate: () => settings.store.showMode === ShowMode.EyeIconRight,
+                    match: /\.Children\.count.+?:null(?<=,channel:(\i).+?)/,
+                    replace: (m, channel) => `${m},$self.isHiddenChannel(${channel})?$self.EyeRightIcon():null`
+                },
+                // Add the hidden lock icon if the channel is hidden
+                {
+                    predicate: () => settings.store.showMode === ShowMode.LockIconRight,
+                    match: /\.Children\.count.+?:null(?<=,channel:(\i).+?)/,
+                    replace: (m, channel) => `${m},$self.isHiddenChannel(${channel})?$self.LockRightIcon():null`
+                },
+            ]
+        },
+        {
+            find: "UNREAD_IMPORTANT:",
+            predicate: () => settings.store.channelStyle === ChannelStyle.Muted || settings.store.channelStyle === ChannelStyle.MutedUnread,
             replacement: [
                 // Make the channel appear as muted if it's hidden
                 {
                     match: /Children\.count.+?;(?=return\(0,\i\.jsxs?\)\(\i\.\i,{focusTarget:)(?<={channel:(\i),name:\i,muted:(\i).+?;)/,
                     replace: (m, channel, muted) => `${m}${muted}=$self.isHiddenChannel(${channel})?true:${muted};`
-                },
-                // Add the hidden eye icon if the channel is hidden
-                {
-                    match: /\.Children\.count.+?:null(?<=,channel:(\i).+?)/,
-                    replace: (m, channel) => `${m},$self.isHiddenChannel(${channel})?$self.HiddenChannelIcon():null`
                 },
                 // Make voice channels also appear as muted if they are muted
                 {
@@ -183,16 +209,14 @@ export default definePlugin({
         },
         {
             find: "UNREAD_IMPORTANT:",
+            predicate: () => settings.store.channelStyle !== ChannelStyle.Unread && settings.store.channelStyle !== ChannelStyle.MutedUnread,
             replacement: [
                 {
-                    // Make muted channels also appear as unread if hide unreads is false, using the HiddenIconWithMutedStyle and the channel is hidden
-                    predicate: () => settings.store.hideUnreads === false && settings.store.showMode === ShowMode.HiddenIconWithMutedStyle,
                     match: /(?<=\.LOCKED;if\()(?<={channel:(\i).+?)/,
                     replace: (_, channel) => `!$self.isHiddenChannel(${channel})&&`
                 },
                 {
                     // Hide unreads
-                    predicate: () => settings.store.hideUnreads === true,
                     match: /Children\.count.+?;(?=return\(0,\i\.jsxs?\)\(\i\.\i,{focusTarget:)(?<={channel:(\i),name:\i,.+?unread:(\i).+?)/,
                     replace: (m, channel, unread) => `${m}${unread}=$self.isHiddenChannel(${channel})?false:${unread};`
                 }
@@ -475,9 +499,19 @@ export default definePlugin({
                 match: /(getVoiceStateForUser.{0,150}?)&&\i\.\i\.canWithPartialContext.{0,20}VIEW_CHANNEL.+?}\)(?=\?)/,
                 replace: "$1"
             }
+        },
+        {
+            find: "#{intl::ROLE_REQUIRED_SINGLE_USER_MESSAGE}",
+            replacement: {
+                match: /(?=function (\i)\(\i\){let{channel:.{0,200}?getSortedRoles\()/,
+                replace: "$self.ChannelBeginHeader=$1;"
+            }
         }
     ],
 
+    set ChannelBeginHeader(value: any) {
+        setChannelBeginHeader(value);
+    },
 
     swapViewChannelWithConnectPermission(mergedPermissions: bigint, channel: Channel) {
         if (!PermissionStore.can(PermissionsBits.CONNECT, channel)) {
@@ -541,7 +575,7 @@ export default definePlugin({
 
     LockIcon: ErrorBoundary.wrap(() => (
         <svg
-            className={ChannelListClasses.icon}
+            className={cl("channel-list-icon")}
             height="18"
             width="20"
             viewBox="0 0 24 24"
@@ -552,13 +586,13 @@ export default definePlugin({
         </svg>
     ), { noop: true }),
 
-    HiddenChannelIcon: ErrorBoundary.wrap(() => (
+    EyeRightIcon: ErrorBoundary.wrap(() => (
         <Tooltip text="Hidden Channel">
             {({ onMouseLeave, onMouseEnter }) => (
                 <svg
                     onMouseLeave={onMouseLeave}
                     onMouseEnter={onMouseEnter}
-                    className={classes(ChannelListClasses.icon, cl("hidden-channel-icon"))}
+                    className={classes(cl("channel-list-icon"), cl("hidden-channel-icon"))}
                     width="24"
                     height="24"
                     viewBox="0 0 24 24"
@@ -566,6 +600,25 @@ export default definePlugin({
                     role="img"
                 >
                     <path fill="currentcolor" fillRule="evenodd" d="m19.8 22.6-4.2-4.15q-.875.275-1.762.413Q12.95 19 12 19q-3.775 0-6.725-2.087Q2.325 14.825 1 11.5q.525-1.325 1.325-2.463Q3.125 7.9 4.15 7L1.4 4.2l1.4-1.4 18.4 18.4ZM12 16q.275 0 .512-.025.238-.025.513-.1l-5.4-5.4q-.075.275-.1.513-.025.237-.025.512 0 1.875 1.312 3.188Q10.125 16 12 16Zm7.3.45-3.175-3.15q.175-.425.275-.862.1-.438.1-.938 0-1.875-1.312-3.188Q13.875 7 12 7q-.5 0-.938.1-.437.1-.862.3L7.65 4.85q1.025-.425 2.1-.638Q10.825 4 12 4q3.775 0 6.725 2.087Q21.675 8.175 23 11.5q-.575 1.475-1.512 2.738Q20.55 15.5 19.3 16.45Zm-4.625-4.6-3-3q.7-.125 1.288.112.587.238 1.012.688.425.45.613 1.038.187.587.087 1.162Z" />
+                </svg>
+            )}
+        </Tooltip>
+    ), { noop: true }),
+
+    LockRightIcon: ErrorBoundary.wrap(() => (
+        <Tooltip text="Hidden Channel">
+            {({ onMouseLeave, onMouseEnter }) => (
+                <svg
+                    onMouseLeave={onMouseLeave}
+                    onMouseEnter={onMouseEnter}
+                    className={cl("channel-list-icon") + " " + "shc-hidden-channel-icon"}
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    aria-hidden={true}
+                    role="img"
+                >
+                    <path fill="currentcolor" className="shc-evenodd-fill-current-color" d="M17 11V7C17 4.243 14.756 2 12 2C9.242 2 7 4.243 7 7V11C5.897 11 5 11.896 5 13V20C5 21.103 5.897 22 7 22H17C18.103 22 19 21.103 19 20V13C19 11.896 18.103 11 17 11ZM12 18C11.172 18 10.5 17.328 10.5 16.5C10.5 15.672 11.172 15 12 15C12.828 15 13.5 15.672 13.5 16.5C13.5 17.328 12.828 18 12 18ZM15 11H9V7C9 5.346 10.346 4 12 4C13.654 4 15 5.346 15 7V11Z" />
                 </svg>
             )}
         </Tooltip>
