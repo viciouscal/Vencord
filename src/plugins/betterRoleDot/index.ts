787bb6054b2eb9d77c9c5df47608ccf29531e765
viciouscal <78273450+viciouscal@users.noreply.github.com>
2026-05-05 19:20:24 +0000

    fix conflict markers

--- src/plugins/betterRoleDot/index.ts
+++ src/plugins/betterRoleDot/index.ts
@@ -41,6 +41,8 @@ export default definePlugin({
     authors: [Devs.Ven, Devs.AutumnVN],
     description:
         "Copy role colour on RoleDot (accessibility setting) click. Also allows using both RoleDot and coloured names simultaneously",
+    tags: ["Roles", "Appearance"],
+    settings,
 
     patches: [
         {
