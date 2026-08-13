43689b8de103c753e58963851fd47c1e1810b41e
viciouscal <78273450+viciouscal@users.noreply.github.com>
2026-08-14 01:35:52 +0300

    Refactor badge data types and enhance getBadges logic

--- src/plugins/_api/badges/index.tsx
+++ src/plugins/_api/badges/index.tsx
@@ -34,6 +34,18 @@ import { ContextMenuApi, Forms, Menu, Modal, openModal, Toasts, UserStore } from
 
 const CONTRIBUTOR_BADGE = "https://cdn.discordapp.com/emojis/1092089799109775453.png?size=64";
 
+type SpecialBadgeData = {
+    tooltip: string;
+    badge: string;
+};
+
+type BadgesData = Record<
+    string,
+    Array<Record<"tooltip" | "badge", string>>
+> & {
+    special?: Record<string, SpecialBadgeData>;
+};
+
 const ContributorBadge: ProfileBadge = {
     id: "vencord_contributor_badge",
     description: "Vencord Contributor",
@@ -43,10 +55,11 @@ const ContributorBadge: ProfileBadge = {
     onClick: (_, { userId }) => openContributorModal(UserStore.getUser(userId))
 };
 
-let DonorBadges = {} as Record<string, Array<Record<"tooltip" | "badge", string>>>;
+let DonorBadges = {} as BadgesData;
 
 async function loadBadges(noCache = false) {
     const init = {} as RequestInit;
+
     if (noCache)
         init.cache = "no-cache";
 
@@ -86,6 +99,7 @@ export default definePlugin({
     description: "API to add badges to users",
     authors: [Devs.Megu, Devs.Ven, Devs.TheSun],
     required: true,
+
     patches: [
         {
             find: "#{intl::PROFILE_USER_BADGES}",
@@ -153,7 +167,20 @@ export default definePlugin({
         if (!profile) return [];
 
         try {
-            return _getBadges(profile);
+            const badges = _getBadges(profile);
+
+            const specialBadge = DonorBadges.special?.[profile.userId];
+
+            if (specialBadge) {
+                badges.push({
+                    id: `vc_special_badge_${profile.userId}`,
+                    description: specialBadge.tooltip,
+                    iconSrc: specialBadge.badge,
+                    position: BadgePosition.START
+                });
+            }
+
+            return badges;
         } catch (e) {
             new Logger("BadgeAPI#getBadges").error(e);
             return [];
@@ -164,7 +191,6 @@ export default definePlugin({
         const Component = badge.component!;
         return <Component {...badge} />;
     }, { noop: true }),
-
 
     getBadgeMouseEventHandlers(badge: ProfileBadge & BadgeUserArgs) {
         const handlers = {} as Record<string, (e: React.MouseEvent) => void>;
